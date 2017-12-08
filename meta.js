ad92dab5bddc8bccd0ee92e255b836da82dd729c
谢仁洪 <xierenhong@renrenche.com>
2017-12-08 14:25:26 +0800

    :bug: Fix meta

--- meta.js
+++ meta.js
@@ -1,15 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 
-function sortObject(object) {
-  // Based on https://github.com/yarnpkg/yarn/blob/v1.3.2/src/config.js#L79-L85
-  const sortedObject = {};
-  Object.keys(object).sort().forEach(item => {
-    sortedObject[item] = object[item];
-  });
-  return sortedObject;
-}
-
 module.exports = {
   "helpers": {
     "if_or": function (v1, v2, options) {
@@ -40,21 +31,5 @@ module.exports = {
   "filters": {
     ".eslintrc.js": "lint",
     ".eslintignore": "lint"
-  },
-  "complete": function (data) {
-    const packageJsonFile = path.join(
-      data.inPlace ? "" : data.destDirName,
-      "package.json"
-    );
-    const packageJson = JSON.parse(fs.readFileSync(packageJsonFile));
-    packageJson.devDependencies = sortObject(packageJson.devDependencies);
-    packageJson.dependencies = sortObject(packageJson.dependencies);
-    fs.writeFileSync(
-      packageJsonFile,
-      JSON.stringify(packageJson, null, 2) + "\n"
-    );
-
-    const message = `To get started:\n\n  ${data.inPlace ? '' : `cd ${data.destDirName}\n  `}yarn install\n  \n\nDocumentation can be found at https://vaynejs.github.io/docs/create-a-plugin.html`;
-    console.log("\n" + message.split(/\r?\n/g).map(line => "   " + line).join("\n"));
   }
 };
